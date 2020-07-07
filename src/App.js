c86962944c9802bdb3dfff3c9e72fef6b76bdb8d
duplxey <nick.tomazic@gmail.com>
2020-07-07 02:43:31 +0200

    Added auth block.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Sidebar from './components/Sidebar'
-import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+
 import Clicks from "./pages/Clicks";
 import Keywords from "./pages/Keywords";
 import Settings from "./pages/Settings";
 import Profiles from "./pages/Profiles";
+import Auth from "./auth/Auth";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Route path="/" exact component={Profiles} />
-        <Route path="/clicks" exact component={Clicks} />
-        <Route path="/keywords" exact component={Keywords} />
-        <Route path="/settings" exact component={Settings} />
-      </Router>
-    </div>
+  let loggedIn = false;
 
-  );
+  if (!loggedIn) {
+    return (
+      <div className="App">
+        <Auth />
+      </div>
+    )
+  } else {
+    return (
+      <div className="App">
+        <Router>
+          <Route path="/" exact component={Profiles} />
+          <Route path="/clicks" exact component={Clicks} />
+          <Route path="/keywords" exact component={Keywords} />
+          <Route path="/settings" exact component={Settings} />
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/auth/Auth.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Auth.css";
+
+const Auth = () => {
+  return (
+    <div className="Auth">
+      <p>Please log in.</p>
+    </div>
+  )
+};
+
+export default Auth;
